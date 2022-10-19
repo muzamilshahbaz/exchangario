62e1139b5660458bed962841f4de84b292fed815
Muzamil Shahbaz <muzamil.shahbaz@stackupsolutions.com>
2022-10-19 18:54:49 +0500

    changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -15,6 +16,6 @@ library.add(faStar)
 
 const app = createApp(App);
 
-app.use(router);
+app.use(store).use(router);
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,71 +1,46 @@
 <template>
-    <div id="exchangario">
-     <ExchangarioHeroSection/>
-      <ExchangarioPost :exchanges="exchanges" />
-      <ExchangarioPagination />
-    </div>
-  </template>
-  
-  <script>
-  import ExchangarioPost from "../components/ExchangarioPost.vue";
-  import ExchangarioPagination from "../components/ExchangarioPagination.vue";
+  <div id="exchangario">
+    <ExchangarioHeroSection />
+    <ExchangarioPost :exchanges="exchanges" />
+    <ExchangarioPagination />
+  </div>
+</template>
+
+<script>
+import ExchangarioPost from "../components/ExchangarioPost.vue";
+import ExchangarioPagination from "../components/ExchangarioPagination.vue";
 import ExchangarioHeroSection from "@/components/ExchangarioHeroSection.vue";
-  
-  export default {
-    components: {
+
+export default {
+  components: {
     ExchangarioPost,
     ExchangarioPagination,
-    ExchangarioHeroSection
-},
-    data() {
-      return {
+    ExchangarioHeroSection,
+  },
 
-        exchanges: [
-          {
-            id: "ad7a1231238dasd",
-            type: "product", // service or product
-            title: "New Tesla Car",
-            description: "I will exchange this nice Tesla car",
-            price: 200,
-            user: "Adam Donald",
-            image:
-              "https://images.unsplash.com/photo-1554744512-d6c603f27c54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=50",
-            country: "Germany",
-            city: "Berlin",
-            tags: ["cars", "electric"],
-          },
-          {
-            id: "ad7asdzxasa68dasd",
-            type: "service", // service or product
-            title: "Programming Lessons",
-            description: "I will make you a super programmer",
-            price: 45, // value per hour in case of service,
-            user: "Ema Watson",
-            image:
-              "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-            country: "Spain",
-            city: "Barcelona",
-            tags: ["programming", "code"],
-          },
-        ],
-      };
+  computed: {
+    exchanges() {
+      return this.$store.state.exchange.items;
     },
-
-   computed: {
-    exchange_length(){
+    exchange_length() {
       return this.exchanges.length;
-    }
-   },
-
-   methods: {
-    log(){
-      console.log('Hello!!');
     },
-    increment(){
-      this.count++;
-    }
-   }
-  }
-  </script>
+  },
 
-  
+  // mounted() {
+    
+  // },
+
+  created() {
+    this.$store.dispatch("exchange/getExchanges")
+  },
+  methods: {
+    log() {
+      console.log("Hello!!");
+    },
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
