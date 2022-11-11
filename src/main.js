9ce4ff0b6013a66454319cfb554fdc11a2090cc7
Muzamil Shahbaz <muzamil.shahbaz@stackupsolutions.com>
2022-11-11 18:17:23 +0500

    changes

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store';
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -14,9 +14,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
+
 library.add(faStar)
 
 const app = createApp(App);
+
+store.dispatch('user/onAuthChange');
 
 app.use(store).use(router).use(Toast);
 app.component('font-awesome-icon', FontAwesomeIcon)
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,7 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { async } from "@firebase/util";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../../db";
 
 export default {
     namespaced: true,
@@ -11,22 +14,50 @@ export default {
         }
     },
     actions: {
-        async register({ commit, dispatch }, { email, password }) {
+        onAuthChange({ dispatch }) {
+            onAuthStateChanged(getAuth(), async(user) => {
+                if (user) {
+                    const userProfile = await dispatch('getUserProfile', user.uid);
+                    console.log(userProfile);
+
+                } else {
+                    console.log('Logged out');
+                }
+            })
+        },
+
+        async getUserProfile(_, id) {
+            const docRef = doc(db, "users", id);
+            const docSnap = await getDoc(docRef);
+            return docSnap.data();
+        },
+
+        async register({ commit, dispatch }, { email, password, username }) {
             commit("setRegisterIsProcessing", true);
             commit("setRegisterError", "");
 
             // Firebase functionality to register user
 
             try {
-                const user_credentials = await createUserWithEmailAndPassword(getAuth(), email, password);
-                return user_credentials.user;
+                const { user } = await createUserWithEmailAndPassword(getAuth(), email, password);
+                await dispatch("createUserProfile", {
+                    id: user.uid,
+                    username,
+                    avatar: "https://www.pinclipart.com/picdir/big/133-1331433_free-user-avatar-icons-happy-flat-design-png.png",
+                    credit: 0,
+                    exchanges: [],
+                });
             } catch (e) {
                 commit("setRegisterError", e.message);
                 dispatch("toast/error", e.message, { root: true });
             } finally {
                 commit("setRegisterIsProcessing", false);
             }
-        }
+        },
+
+        async createUserProfile(_, { id, ...profile }) {
+            await setDoc(doc(db, "users", id), profile)
+        },
     },
     mutations: {
         setRegisterIsProcessing(state, isProcessing) {
